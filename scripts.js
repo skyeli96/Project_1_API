2953977f4d75ff3d91ca42943e1577cd0c62a7bf
Chee Quee <mitchellcq@gmail.com>
2020-09-24 19:25:44 +1000

    added randomisation

--- scripts.js
+++ scripts.js
@@ -8,15 +8,15 @@ function foodSearchUrl() {
 
     var appKey = "65bc38bb4c3b484861c57fb9690d8edc";
     var appId = "bb8f4a04";
-    var queryUrl = "https://api.edamam.com/search?q=" + ingredients + "&app_id=" + appId + "&app_key=" + appKey + "&from=0&to=2";
+    var queryUrl = "https://api.edamam.com/search?q=" + ingredients + "&app_id=" + appId + "&app_key=" + appKey + "&from=0&to=20";
 
     return queryUrl;
 }
 
 //dynamically builds 2 card elements on the page
 function recipeCard(obj) {
-    for (let i = 0; i < obj.hits.length; i++) {
-        var recipe = obj.hits[i].recipe;
+    for (let i = 0; i < 2; i++) {
+        var recipe = obj.hits[random(obj.hits.length)].recipe;
 
         var appendFood = $("#appendFood");
 
@@ -28,7 +28,7 @@ function recipeCard(obj) {
         var container = $('<div class="container">');
         var recipeName = $('<h4 style="font-weight:bold">');
         var recipeDescription = $('<p style="font-size:15px">');
-        var button = $('<button type="button" class="btn btn-primary">');
+        var button = $('<button type="button" class="btn btn-primary favourite">');
 
         //sets the attributes of some of the new elements
         link.attr("href", recipe.url);
@@ -52,6 +52,12 @@ function recipeCard(obj) {
     }
 };
 
+//creates 2 random numbers between 1 and 20
+function random(length) {
+    var number = Math.floor(Math.random() * length);
+    return number;
+}
+
 // takes an array and spearates the contents to allow proper display
 function parseArray(array) {
     var parsedArray = array.join(", ");
@@ -67,7 +73,9 @@ $("#searchFood").on("click", function (event) {
 
     //adds ingredients to search history
     ingredients.push(foodInput.val());
+    console.log("---Ingredients Array---");
     console.log(ingredients);
+    console.log("--- --- ---");
     searchHistory.val(parseArray(ingredients));
 
     var url = foodSearchUrl();
@@ -91,4 +99,23 @@ $("#clear").on("click", function (event) {
     ingredients.length = 0;
     console.log(ingredients);
     searchHistory.val("");
+});
+
+//
+$(document).on({
+    ajaxStart: function () {
+        $("#appendFood").addClass("loader");
+    },
+    ajaxStop: function () {
+        $("#appendFood").removeClass("loader");
+    }
+});
+
+//event listener for add to favourites button
+$("#favourite").on("click", function (e) {
+    e.preventDefault();
+    localStorage.setItem("ingredients", Ingredients);
+    console.log('--- Local Storage ---');
+    console.log(localStorage);
+    console.log('--- --- ---');
 })
